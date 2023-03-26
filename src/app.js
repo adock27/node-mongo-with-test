5b37eed0b43ef5d4f08f983c0a7c42362cc59044
webmaster-alcartagena <webmaster@cartagena.gov.co>
2023-03-25 19:31:47 -0500

    User Added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const _connect = require('./db/_connect');
 require('dotenv').config();
+
 const blogRouter = require("./routes/BlogRoutes");
+const userRouter = require("./routes/UserRoutes");
 
 
 // mongo db connection 
@@ -14,6 +16,7 @@ app.use(express.json());
 
 
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
 
  
 app.listen(3001, () => {
