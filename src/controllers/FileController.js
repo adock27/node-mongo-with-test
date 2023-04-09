862815a0456642d80425a696ba4cf7fcb0f2a766
adock27 <andersondumet@gmail.com>
2023-04-08 19:11:23 -0500

    file added and file remove working to react app

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,10 +1,14 @@
 // Importamos la biblioteca "multer" que se utiliza para manejar la carga de archivos.
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
+
+
+
 // Creamos un objeto "storage" que especifica cómo y dónde se guardarán los archivos que se cargan utilizando multer.
 const storage = multer.diskStorage({
     // La propiedad "destination" especifica la carpeta donde se guardarán los archivos.
-    destination: path.join(__dirname,'../public/uploads'),
+    destination: path.join(__dirname, '../public/uploads'),
     // La propiedad "filename" establece el nombre del archivo.
     filename: function (req, file, cb) {
         // En este caso, el nombre del archivo se compone de la marca de tiempo actual y el nombre original del archivo.
@@ -15,7 +19,7 @@ const storage = multer.diskStorage({
 // Inicializamos un objeto "upload" con la configuración de almacenamiento de archivos.
 const upload = multer({
     storage: storage,
-    dest: path.join(__dirname,'public/uploads'),
+    dest: path.join(__dirname, 'public/uploads'),
     fileFilter: (req, file, cb) => {
         const fileTypes = /jpeg|jpg|png|gif/;
         const mimeType = fileTypes.test(file.mimetype);
@@ -36,6 +40,29 @@ exports.upload = upload.single('file');
 exports.uploadFile = (req, res) => {
     console.log(req.file);
     res.send({
-        path: `/uploads/${req.file.filename}`
+        path: `http://localhost:3001/uploads/${req.file.filename}`,
+        filepath: req.file.path
     })
+}
+// Exportamos una función "uploadFile" que se utilizará para manejar la carga de archivos y para realizar cualquier otra acción necesaria en la ruta de carga.
+exports.removeFile = (req, res) => {
+
+    const path = req.body.path
+    // console.log(path);
+
+    fs.unlink(path, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+
+        //file removed
+
+        res.send({
+           response: 'delete'
+        })
+    })
+
+
+
 }
--- src/routes/FileRoutes.js
+++ src/routes/FileRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
-const { upload, uploadFile } = require('../controllers/FileController');
+
+const { upload, uploadFile, removeFile } = require('../controllers/FileController');
 
 const router = express.Router();
 
 router.route("/").post(upload, uploadFile);
+router.route("/remove").post(removeFile);
+
 
 module.exports = router;
