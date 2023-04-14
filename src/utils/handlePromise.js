7b8c399ec5f63724af84f3ac1cc7667bce0df5f5
adock27 <andersondumet@gmail.com>
2023-04-13 23:38:36 -0500

    data handlet fixed

--- src/utils/handlePromise.js
+++ src/utils/handlePromise.js
@@ -1,7 +1,7 @@
 module.exports = (promise, res) => {
     promise
         .then(result => {
-            res.json({ data: result, status: "success" });
+            res.status(200).json(...result);
         })
         .catch(error => {
             res.status(500).json({ error: error.message });
