7003e31f3ca1e47781d824ad2044483f7939d099
adock27 <andersondumet@gmail.com>
2023-04-13 23:51:09 -0500

    data fixed not spread

--- src/utils/handlePromise.js
+++ src/utils/handlePromise.js
@@ -1,7 +1,7 @@
 module.exports = (promise, res) => {
     promise
         .then(result => {
-            res.status(200).json(...result);
+            res.status(200).json(result);
         })
         .catch(error => {
             res.status(500).json({ error: error.message });
